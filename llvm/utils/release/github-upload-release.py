5c992352bba78854ffa05a825d977813d5aa1c1f
KomiMoe <i@komi.moe>
2024-09-23 20:15:41 +0800

    fix release py

--- llvm/utils/release/github-upload-release.py
+++ llvm/utils/release/github-upload-release.py
@@ -36,19 +36,19 @@ from textwrap import dedent
 
 def create_release(repo, release, tag=None, name=None, message=None):
     if not tag:
-        tag = "llvmorg-{}".format(release)
+        tag = "{}".format(release)
 
     if not name:
-        name = "LLVM {}".format(release)
+        name = "{}".format(release)
 
     if not message:
         message = dedent(
             """\
-            LLVM {} Release
+            Arkari {} Release
 
             # A note on binaries
 
-            Volunteers make binaries for the LLVM project, which will be uploaded
+            Volunteers make binaries for the Arkari project, which will be uploaded
             when they have had time to test and build these binaries. They might
             not be available directly or not at all for each release. We suggest
             you use the binaries from your distribution or build your own if you
@@ -85,8 +85,7 @@ parser.add_argument("--files", nargs="+", type=str)
 args = parser.parse_args()
 
 gh = github.Github(args.token)
-llvm_org = gh.get_organization("llvm")
-llvm_repo = llvm_org.get_repo("llvm-project")
+arkari_repo = gh.get_user().get_repo("Arkari")
 
 if args.user:
     if not args.user_token:
@@ -94,19 +93,14 @@ if args.user:
         sys.exit(1)
     # Validate that this user is allowed to modify releases.
     user = gh.get_user(args.user)
-    team = (
-        github.Github(args.user_token)
-        .get_organization("llvm")
-        .get_team_by_slug("llvm-release-managers")
-    )
-    if not team.has_in_members(user):
-        print("User {} is not a allowed to modify releases".format(args.user))
+    if user.login != gh.get_user().login:
+        print("User {} is not allowed to modify releases".format(args.user))
         sys.exit(1)
 elif args.command == "check-permissions":
     print("--user option required for check-permissions")
     sys.exit(1)
 
 if args.command == "create":
-    create_release(llvm_repo, args.release)
+    create_release(arkari_repo, args.release)
 if args.command == "upload":
-    upload_files(llvm_repo, args.release, args.files)
+    upload_files(arkari_repo, args.release, args.files)
